ccc2c10b44296db78d71e7b0741e33e9b96d1fa5
Vanji <vikashvanchi@gmail.com>
2026-04-04 19:57:18 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        subsets=[]
+        backpack=[]
+        #goal is to remove duplicate subsets, that is the number of elements is the one
+        #we track for each element, order does not matter, so to keep same number
+        #of numbers in each element.....idk the logic for that
+        nums.sort()
+        def dfs(i):
+
+            if i>=len(nums):
+                subsets.append(backpack[:])
+                return
+            
+            backpack.append(nums[i])
+            dfs(i+1)
+
+            while i+1 <len(nums) and nums[i+1]==nums[i]:
+                i+=1
+            backpack.pop()
+
+            dfs(i+1)
+        
+        dfs(0)
+        return subsets
+
+            
